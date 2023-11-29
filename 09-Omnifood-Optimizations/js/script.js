c30e511cd94fd6bb8e9771cf876189a01f121f49
hamzabilalaydin <hamzaba61@gmail.com>
2023-11-29 15:06:58 +0300

    Sticky navigation added

--- 09-Omnifood-Optimizations/js/script.js
+++ 09-Omnifood-Optimizations/js/script.js
@@ -12,6 +12,30 @@ console.log(name);
 // });
 
 ///////////////////////////////////////////////////////////
+//Sticky Navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(function(entries){
+  const ent = entries[0];
+  console.log(ent);
+
+  if(ent.isIntersecting === false) {
+    document.body.classList.add("sticky");
+  }
+
+  if(ent.isIntersecting) {
+    document.body.classList.remove("sticky");
+  }
+
+},
+ {
+  //In the viewport
+  root: null,
+  threshold: 0,
+  rootMargin: '-80px',
+});
+obs.observe(sectionHeroEl);
+///////////////////////////////////////////////////////////
 //Set cuurent year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
